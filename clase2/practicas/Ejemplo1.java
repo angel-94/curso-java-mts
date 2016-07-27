af27c0d840d6948b761295789dff801aeaf7342e
Meltsan <meltsan@localhost.localdomain>
2016-07-27 17:12:25 -0500

    Clase 2

--- clase2/practicas/Ejemplo1.java
+++ clase2/practicas/Ejemplo1.java
@@ -6,68 +6,26 @@ public class Ejemplo1{
 	private static final String PASS = "123";
 
 	public static void main(String [] args){
-		
+		String username = "";
+		String pass = "";
 		BufferedReader dataIn = new BufferedReader( new InputStreamReader(System.in));
-		
-		int grade = 0;
-		String var1 = "";		
-	
-		System.out.println("Gados: ");
-		try{
-			grade = Integer.parseInt(dataIn.readLine());
-			/*
- 			* 60-70 = c
- 			* */
-			/*if(grade > 60){
-				System.out.println("Felicidades");
-			}else{
-				System.out.println("No pasaste");
-			}*/
 			
-			if(grade >= 60 && grade <= 70){
-				var1 = "C";
-				//System.out.println("C");
-			}else if(grade >= 71 && grade <= 80){
-				var1 = "B";
-				//System.out.println("B");
-			}else if(grade >= 81 && grade <= 90){
-				var1 = "A";
-				//System.out.println("A");
-			}else if(grade >= 91 && grade <= 98){
-				var1 = "A+";
-				//System.out.println("A+");
-			}else if(grade == 99 || grade == 100){
-				var1 = "S";
-				//System.out.println("S");
+		System.out.println("Username: ");
+		try{
+			username = dataIn.readLine();
+			if(username.isEmpty()){
+				System.out.println("Error de nombre");
+				return;
+			}		
+			System.out.println("Password: ");
+			pass = dataIn.readLine();
+			if(!PASS.equals(pass)){
+				System.out.println("Datos incorrectos");
 			}else{
-				System.out.println("Fuera de rango");
-			}
-			switch(var1){
-				case "C":  
-					System.out.println("De pansazo");
-				break;
-				case "B":
-					System.out.println("Nice");
-				break;
-				case "A":
-					System.out.println("Mension HOnorifica");
-				break;
-				case "A+":
-					System.out.println("Mension Honorifica");
-				break;
-				case "S":
-					System.out.println("Mension Honorifica");
-				break;
-				default:
-					System.out.println("Este es un defaukt");
+				System.out.println("Bienvenido " + username);
 			}
 		}catch(IOException e){
 			System.out.println("Error en leer");
-		}catch(NumberFormatException e){ 
-			System.out.println("Se esperaba un numero");
-		}catch(Exception e){
-			System.out.println("Error desconocido");
 		}
-		//System.out.println(mensaje);
 	}
 }
---
+++ clase2/practicas/Ejemplo2.java
@@ -0,0 +1,81 @@
+import java.io.*;
+import java.lang.*;
+
+public class Ejemplo1{
+
+	private static final String PASS = "123";
+
+	public static void main(String [] args){
+		
+		BufferedReader dataIn = new BufferedReader( new InputStreamReader(System.in));
+		
+		int grade = 0;
+		String var1 = "";		
+	
+		System.out.println("Gados: ");
+		try{
+			grade = Integer.parseInt(dataIn.readLine());
+			/*
+ 			* 60-70 = c
+ 			* */
+			/*if(grade > 60){
+				System.out.println("Felicidades");
+			}else{
+				System.out.println("No pasaste");
+			}*/
+			
+			if(grade >= 60 && grade <= 70){
+				var1 = "C";
+				//System.out.println("C");
+			}else if(grade >= 71 && grade <= 80){
+				var1 = "B";
+				//System.out.println("B");
+			}else if(grade >= 81 && grade <= 90){
+				var1 = "A";
+				//System.out.println("A");
+			}else if(grade >= 91 && grade <= 98){
+				var1 = "A+";
+				//System.out.println("A+");
+			}else if(grade == 99 || grade == 100){
+				var1 = "S";
+				//System.out.println("S");
+			}else{
+				System.out.println("Fuera de rango");
+			}
+			/*while(grade <= 100){
+				System.out.println("Grado: " + grade);
+				grade++;
+			}*/
+			do{
+				System.out.println("Grado: "+ grade);
+				grade++;
+			}while(grade <= 100);
+			switch(var1){
+				case "C":  
+					System.out.println("De pansazo");
+				break;
+				case "B":
+					System.out.println("Nice");
+				break;
+				case "A":
+					System.out.println("Mension HOnorifica");
+				break;
+				case "A+":
+					System.out.println("Mension Honorifica");
+				break;
+				case "S":
+					System.out.println("Mension Honorifica");
+				break;
+				default:
+					System.out.println("Este es un defaukt");
+			}
+		}catch(IOException e){
+			System.out.println("Error en leer");
+		}catch(NumberFormatException e){ 
+			System.out.println("Se esperaba un numero");
+		}catch(Exception e){
+			System.out.println("Error desconocido");
+		}
+		//System.out.println(mensaje);
+	}
+}
